9065411a7887df623d63752f7f8c08284f3a3c6c
HarunHashim <harunmwondi@MSI.localdomain>
2026-03-25 13:01:48 -0400

    Made the changes related to the data types . Yet to test on the pipeline to examine if it works all through

--- QNX_files/src/data_parser.c
+++ QNX_files/src/data_parser.c
@@ -16,31 +16,51 @@
 
 
 // Harun - Will use this in other files to initialize struct - to ensure whatever isnt used is auto set to default
-
+// Might not use at the moment
 void packet_init(msg_packet *c)
 {
+    strcpy(c->origin, "");
     strcpy(c->subsys, "");
-    strcpy(c->msg_type, "");
     c->msg.percentage = -1.0;
     c->msg.angle = -1.0;
-    c->msg.enabled = 0;  // Evaluates to false
+    c->msg.enabled = 0;  
+    c->msg.speed = -1.0;
 }
+
+//Have to use as memset values correspond to expected values in webots
+//This just initializes the struct with the default values to symbolize it empty or unchanged . For now im using the value -9999 (OUT OF RANGE)
+void vc_init(vehicle_controls *vc){
+    strcpy(vc->subsys, "");
+    strcpy(vc->data.toggleGear, "");
+    vc->data.throttle_level = -9999;
+    vc->data.brake_level = -9999;
+    vc->data.steering_level = -9999;  
+}
+
 
 // telemetry struct → JSON to send to Python
 char *telemetry_to_json(telemetry_packet *t) {
     cJSON *root = cJSON_CreateObject();
     cJSON *data = cJSON_CreateObject();
-
+    cJSON *warnings_arr = cJSON_CreateArray();
     
     cJSON_AddStringToObject(root, "type",      t->type);
     
 
     cJSON_AddNumberToObject(data, "Speed",         t->tel.speed);
-    // cJSON_AddNumberToObject(data, "Throttle",      t->tel.throttle);
-    // cJSON_AddNumberToObject(data, "Brake",         t->tel.brake);
-    // cJSON_AddNumberToObject(data, "SteeringAngle", t->tel.steering_angle);
-    // cJSON_AddBoolToObject  (data, "SafeMode",      t->tel.safe_mode);
-    // cJSON_AddBoolToObject  (data, "SnowMode",      t->tel.snow_mode);
+    cJSON_AddBoolToObject  (data, "SnowMode",      t->tel.snow_mode);
+
+    //This will be for the warnings part of it 
+    //Will involve parsing the list of warnings into an array of strings then idk ho we will work on that , dashboard to decide
+
+    // add warnings array to JSON
+    
+    for (int i = 0; i < t->tel.warning_count; i++) {
+        cJSON_AddItemToArray(warnings_arr, 
+                            cJSON_CreateString(t->tel.warnings[i]));
+    }
+    cJSON_AddItemToObject(data, "Warnings", warnings_arr);
+
 
     cJSON_AddItemToObject(root, "data", data);
     char *out = cJSON_PrintUnformatted(root);
@@ -66,7 +86,7 @@ void json_to_msg_packet(const char *json_str, msg_packet *p) {
     cJSON *data = cJSON_GetObjectItemCaseSensitive(root, "data");
 
     if (cJSON_IsString(origin))
-        strncpy(p->msg_type, type->valuestring, sizeof(p->msg_type) - 1);
+        strncpy(p->origin, type->valuestring, sizeof(p->origin) - 1);
 
     if (cJSON_IsString(type))
         strncpy(p->msg_type, type->valuestring, sizeof(p->msg_type) - 1);
@@ -102,12 +122,25 @@ New archtype for dtaa handling all data being received into qnx
 */
 
 //This fuction is for parsing data being sent to webot sim
-char* sim_data_to_json(vehicle_controls v*){
+char* sim_data_to_json(vehicle_controls *v){
     cJSON *root = cJSON_CreateObject();
-    cJSON_AddNumberToObject(root, "throttle_level",  v->throttle_level);
-    cJSON_AddNumberToObject(root, "brake_level",  v->brake_level);
-    cJSON_AddNumberToObject(root, "steering_level",  v->steering_level);
-    cJSON_AddNumberToObject(root, "toggleGear",  v->toggleGear);
+    cJSON *data = cJSON_CreateObject();
+
+    cJSON_AddStringToObject(root, "subsys",      v->subsys);
+    
+
+    //This basically auto populates each field to null or -999 value that way it only parses in the value required
+    if (v->data.throttle_level != -999) 
+        cJSON_AddNumberToObject(data, "throttle_level", v->data.throttle_level);
+    if (v->data.brake_level != -999)    
+        cJSON_AddNumberToObject(data, "brake_level",    v->data.brake_level);
+    if (v->steering_level != -999) 
+        cJSON_AddNumberToObject(data, "steering_level", v->data.steering_level);
+      
+    cJSON_AddStringToObject(data, "toggleGear",     v->data.toggleGear);
+
+
+    cJSON_AddItemToObject(root, "data", data);
     
     char *out = cJSON_PrintUnformatted(root);
     cJSON_Delete(root);
@@ -119,3 +152,4 @@ char* sim_data_to_json(vehicle_controls v*){
 // Will have to create a data parser for the data coming in from the webots side and going out of it 
 
 // March 24 work doen in telem last --> Harun Mwondi
+
